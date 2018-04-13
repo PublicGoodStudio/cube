1d4db4998ebd762040f1b5b9b921768fffe963c1
Matthew Phillips <matthewphillips@Matthews-MacBook-Pro.local>
2018-04-13 12:35:19 -0600

    trying a flower background

--- js/client.js
+++ js/client.js
@@ -1,13 +1,11 @@
-var ws = new WebSocket('ws://127.0.0.1:8889');
+var ws = new WebSocket('ws://192.168.66.101:8889');
 
   ws.onopen = function open() {
     console.log('connected to server')
   };
 
   ws.onmessage = function incoming(data, flags) {
-    console.log('received a message! ' + JSON.stringify(data));
-
     var image_to_scroll = new Image();
-    image_to_scroll.src = 'img/polygons.png';
+    image_to_scroll.src = 'img/flow-er.png';
     document.body.appendChild(image_to_scroll);
 }
